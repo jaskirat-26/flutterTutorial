268724f6ddc4388ad19a9ad02e68094990efd49d
jaskirat <jaskiratsingh026@gmail.com>
2024-03-06 11:34:31 +0530

    mini project done

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,114 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    home: Home(),
-  ));
-}
+void main() => runApp(MaterialApp(
+  home: NinjaCard(),
+));
 
-class Home  extends StatelessWidget {
-  const Home ({super.key});
-
+class NinjaCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: const Text("My meow meow"),
+        title: const Text('Ninja ID Card', 
+          style: TextStyle(
+            color: Colors.blueGrey,
+            fontFamily: "fonts/PermanentMarker-Regular.ttf"),),
         centerTitle: true,
-        backgroundColor: Colors.amber[400],
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
       ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
-        margin : const EdgeInsets.all(30),
-        color: Colors.grey[300],
-        child: const Text("Hello"),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Center(
+              child: CircleAvatar(
+                radius: 40.0,
+                backgroundImage: AssetImage('assets/tojiPaji.jpg'),
+              ),
+            ),
+            Divider(
+              color: Colors.grey[800],
+              height: 60.0,
+            ),
+            const Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Text(
+              'Chun-Li',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                fontWeight: FontWeight.bold,
+                fontSize: 28.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(height: 30.0),
+            const Text(
+              'HOMETOWN',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Text(
+              'Beijing, China',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                fontWeight: FontWeight.bold,
+                fontSize: 28.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            
+            const SizedBox(height: 30.0),
+            const Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+
+            const SizedBox(height: 10.0),
+            Text(
+              '8',
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                fontWeight: FontWeight.bold,
+                fontSize: 28.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                const SizedBox(width: 10.0),
+                Text(
+                  'chun.li@thenetninja.co.uk',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
-      
-      floatingActionButton: FloatingActionButton(
-        onPressed:() => {
-          debugPrint("Meow")
-        },
-        backgroundColor: Colors.amber[400],
-        child: const Text("button"),),
     );
   }
 }
